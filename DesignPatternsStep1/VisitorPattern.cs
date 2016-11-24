5ca636bb0774e4d4c67e897679e16d03b0cec2af
Matthijs\Pasibun <matthijs_hovestad@hotmail.com>
2016-11-24 16:09:11 +0100

    Step 4, start visitor pattern.

---
+++ DesignPatternsStep1/VisitorPattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsStep1
+{
+    abstract class VisitorPattern
+    {
+
+    }
+
+    class Resize : VisitorPattern {
+    }
+
+    class Move : VisitorPattern {
+    }
+
+    class FileIo : VisitorPattern{
+    }
+}
